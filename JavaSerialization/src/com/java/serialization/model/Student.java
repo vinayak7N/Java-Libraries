4c5afbf4e9f7ffe9a13a716c833db42a44452fd9
Vinayak Nair <vinayaknair1996@gmail.com>
2020-08-07 12:20:55 +0530

    Add files via upload

--- JavaSerialization/src/com/java/serialization/model/Student.java
+++ JavaSerialization/src/com/java/serialization/model/Student.java
@@ -4,7 +4,14 @@ import java.io.Serializable;
 
 public class Student implements Serializable {
 
-    int id =101;
-    String name ="Harry";
+    int id = 101;
+    String name = "Harry";
 
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
